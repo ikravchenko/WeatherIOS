5067b9d954c019a6bf367ca51f5f132045c0b500
Ivan Kravchenko <ikravchenko@minint-b4657v3.stacobon.dd13.mydorma.com>
2014-06-26 12:11:02 +0200

    ínitial commit

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, NSXMLParserDelegate {
+    
+    var weatherEntries: Array<WeatherEntry> = Array<WeatherEntry>()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        downloadWeather()
+    }
+    
+    override func loadView() {
+        var tableView = UITableView()
+        self.view = tableView;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func downloadWeather() {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var url: NSURL = NSURL(string: "http://api.openweathermap.org/data/2.5/forecast?q=Bonn&mode=xml&APPID=7732b247cdb20b941ea963a87e8b8269")
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            println("Task completed")
+            if(error) {
+                // If there is an error in the web request, print it to the console
+                println(error.localizedDescription)
+            }
+            
+            var parser = NSXMLParser(data: data)
+            parser.shouldProcessNamespaces = false
+            parser.shouldReportNamespacePrefixes = false
+            parser.shouldResolveExternalEntities = false
+            parser.delegate = self
+            if !parser.parse() {
+                println("XML Error")
+            }
+            dispatch_async(dispatch_get_main_queue(), {
+//                self.tableData = results
+//                self.appsTableView.reloadData()
+                })
+            })
+        task.resume()
     }
-
-
+    
+    class WeatherEntry : NSObject {
+        var from: String?
+        var to: String?
+        var name: String?
+        var temperature: Float?
+        var isWarm : Bool?
+    }
+    
+    var current: WeatherEntry?
+    
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!,attributes attributeDict: NSDictionary!) {
+        if (elementName == "time") {
+            current = WeatherEntry()
+            current!.from = attributeDict["from"] as NSString
+            current!.to = attributeDict["to"] as NSString
+        } else if (elementName == "symbol") {
+            if (current != nil) {
+                current!.name = attributeDict["name"] as NSString
+            }
+        } else if (elementName == "temperature") {
+            if (current != nil) {
+                current!.temperature = attributeDict["value"]?.floatValue
+                current!.isWarm = current!.temperature? > 15
+            }
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+        if (elementName == "time" && current != nil) {
+            weatherEntries.append(current!)
+        }
+    }
+    
+    func parserDidEndDocument(parser: NSXMLParser!) {
+//        for  w in weatherEntries {
+//            println(w.name)
+//            println(w.from)
+//            println(w.to)
+//            println(w.temperature)
+//            println(w.isWarm)
+//        }
+    }
+    
+    
 }
 
