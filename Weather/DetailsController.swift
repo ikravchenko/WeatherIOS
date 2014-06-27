873ffbf8246b0bf31c2c053638131933b590b5fc
Ivan Kravchenko <ikravchenko@debonw16002.stacobon.dd13.mydorma.com>
2014-06-27 09:44:03 +0200

    added hierarchy for detail and scan

---
+++ Weather/DetailsController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsController.swift
+//  Weather
+//
+//  Created by Ivan Kravchenko on 27/06/14.
+//  Copyright (c) 2014 DORMA. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class DetailsController : UITableViewController {
+    @IBAction func entryEditingCancelled(sender: AnyObject) {
+    }
+    
+    @IBAction func entryEditingDone(sender: AnyObject) {
+    }
+}
